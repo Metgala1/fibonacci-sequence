9b977efac1c2d48bb3cd1d4e55062a24f567938f
ATAGWE ROGER <atagweroger2@gmail.com>
2025-02-21 21:16:36 +0100

    Wrote the functions for the mergesort and fib sequence

---
+++ merge sort/index.js
@@ -0,0 +1,36 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
+}
+
+function merge(left, right) {
+  const merged = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      merged.push(left[i]);
+      i++;
+    } else {
+      merged.push(right[j]);
+      j++;
+    }
+  }
+  return merged.concat(left.slice(i)).concat(right.slice(j));
+}
+
+const array1 = [3, 2, 1, 13, 8, 5, 0, 1];
+const array2 = [105, 79, 100, 110];
+
+console.log(mergeSort(array1))
